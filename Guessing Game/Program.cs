1ae3d4ba25f3205a5a954a8726c901370df3c818
arslanah99 <Arah09@hotmail.com>
2019-06-04 14:53:07 -0600

    allowing for only certian number of guesses

--- Guessing Game/Program.cs
+++ Guessing Game/Program.cs
@@ -9,13 +9,28 @@ namespace Guessing_Game
         {
             string secretWord = "giraffe";
             string guess = "";
+            int guessCount = 0;
+            int guessLimt = 3;
+            bool outOfGuesses = false;
 
-            while(guess != secretWord)
+            while(guess != secretWord && !outOfGuesses)
             {
+                if(guessCount < guessLimit){
                 Console.Write("Enter guess: ");
                 guess = Console.ReadLine();
+                guessCount ++;
+                } else
+                {
+                    outOfGuesses = true;
+                }
+
             }
+            if(outOfGuesses)
+            {
+            Console.Write("You Lose!");
+            } else {
             Console.Write("You Win!");
+            }
 
             Console.ReadLine();
         }
